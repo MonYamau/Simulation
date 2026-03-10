80e2eb0f3e40e9498d090823a10d39bf0672c7fc
MonYamau <White.Fox1825@yandex.ru>
2026-03-10 15:44:30 +0500

    refactoring: add method initializeEntityCreators; rename methods

--- src/main/java/entity/EntityFactory.java
+++ src/main/java/entity/EntityFactory.java
@@ -1,7 +1,6 @@
 package main.java.entity;
 
-import main.java.service.FeedingService;
-import main.java.service.PathFindingService;
+import main.java.service.*;
 import main.java.utils.Coordinates;
 
 import java.util.HashMap;
@@ -11,21 +10,28 @@ import java.util.function.Function;
 public class EntityFactory {
     private final Map<Class<?>, Function<Coordinates, Entity>> entityCreators = new HashMap<>();
 
-    public EntityFactory(PathFindingService bfsPathFinder, FeedingService survivorFeeder, FeedingService predatorFeeder) {
-        writeEntity(Box.class, coordinates -> new Box());
-        writeEntity(Basket.class, coordinates -> new Basket());
-        writeEntity(Cheese.class, coordinates -> new Cheese());
-        writeEntity(Cat.class, coordinates -> new Cat(10, 3, Mouse.class, coordinates, bfsPathFinder, predatorFeeder, 1));
-        writeEntity(Mouse.class, coordinates -> new Mouse(6, 2, Cheese.class, coordinates, bfsPathFinder, survivorFeeder));
-    }
-
-    private <T extends Entity> void writeEntity(Class<T> entityClass, Function<Coordinates, Entity> creator) {
-        entityCreators.put(entityClass, creator);
+    public EntityFactory() {
+        PathFindingService bfsPathFinder = new BfsPathFinder();
+        FeedingService survivorFeeder = new SurvivorFeeder();
+        FeedingService predatorFeeder = new PredatorFeeder();
+        initializeEntityCreators(bfsPathFinder, survivorFeeder, predatorFeeder);
     }
 
     public <T extends Entity> T createEntity(Coordinates coordinates, Class<T> entityClass) {
         @SuppressWarnings("unchecked")
         Function<Coordinates, T> entityCreator = (Function<Coordinates, T>) entityCreators.get(entityClass);
         return entityCreator.apply(coordinates);
+    }
+
+    private void initializeEntityCreators(PathFindingService bfsPathFinder, FeedingService survivorFeeder, FeedingService predatorFeeder) {
+        registerEntityCreator(Box.class, coordinates -> new Box());
+        registerEntityCreator(Basket.class, coordinates -> new Basket());
+        registerEntityCreator(Cheese.class, coordinates -> new Cheese());
+        registerEntityCreator(Cat.class, coordinates -> new Cat(10, 3, Mouse.class, coordinates, bfsPathFinder, predatorFeeder, 2));
+        registerEntityCreator(Mouse.class, coordinates -> new Mouse(6, 2, Cheese.class, coordinates, bfsPathFinder, survivorFeeder));
+    }
+
+    private <T extends Entity> void registerEntityCreator(Class<T> entityClass, Function<Coordinates, Entity> creator) {
+        entityCreators.put(entityClass, creator);
     }
 }
