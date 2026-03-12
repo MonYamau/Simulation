e6de124b7451e5260459135c99309bb654c08660
MonYamau <White.Fox1825@yandex.ru>
2026-03-12 14:09:33 +0500

    refactoring: add SimulationThreadManager.java

--- src/main/java/core/Simulation.java
+++ src/main/java/core/Simulation.java
@@ -3,18 +3,13 @@ package main.java.core;
 import main.java.action.ActionManager;
 import main.java.map.GameMap;
 
-import static main.java.utils.SimulationConstants.THREAD_SLEEP;
-
 public class Simulation {
-    private final TurnExecutor turnExecutor;
-    private volatile boolean isRunning;
-    private volatile boolean isPaused;
-    private Thread simulationThread;
+    TurnExecutor turnExecutor;
+    SimulationThreadManager simulationThreadManager;
 
     public Simulation(GameMap gameMap, ActionManager actionManager) {
-        isRunning = false;
-        isPaused = true;
         this.turnExecutor = new TurnExecutor(gameMap, actionManager);
+        this.simulationThreadManager = new SimulationThreadManager(turnExecutor);
         SimulationCreator.init(gameMap, actionManager);
     }
 
@@ -22,67 +17,15 @@ public class Simulation {
         turnExecutor.nextTurn();
     }
 
-    public synchronized void startSimulation() {
-        if (simulationThread != null && simulationThread.isAlive()) {
-            isPaused = false;
-            notifyAll();
-            return;
-        }
-        isRunning = true;
-        isPaused = false;
-        simulationThread = getThread();
-        simulationThread.start();
+    public void startSimulation() {
+        simulationThreadManager.start();
     }
 
-    public synchronized void pauseSimulation() {
-        isPaused = true;
-        notifyAll();
+    public void pauseSimulation() {
+        simulationThreadManager.pause();
     }
 
-    public synchronized void stopSimulation() {
-        isRunning = false;
-        isPaused = false;
-        notifyAll();
-        if (simulationThread != null) {
-            try {
-                simulationThread.join(1000);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        }
-    }
-
-    @SuppressWarnings("BusyWait")
-    private Thread getThread() {
-        return new Thread(() -> {
-            while (isRunning) {
-                synchronized (this) {
-                    while (isPaused) {
-                        try {
-                            wait();
-                        } catch (InterruptedException e) {
-                            Thread.currentThread().interrupt();
-                            isRunning = false;
-                            break;
-                        }
-                    }
-                }
-                if (!isRunning) break;
-                try {
-                    turnExecutor.nextTurn();
-                } catch (Exception e) {
-                    System.err.println("Error was received when executing the flow: " + e.getMessage());
-                    isRunning = false;
-                    break;
-                }
-                try {
-                    Thread.sleep(THREAD_SLEEP);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    isRunning = false;
-                    break;
-                }
-            }
-        });
+    public void stopSimulation() {
+        simulationThreadManager.stop();
     }
 }
