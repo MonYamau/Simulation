68d1ce565f65cae9edd8f61681b8a365a86ab97c
MonYamau <White.Fox1825@yandex.ru>
2026-03-13 20:38:57 +0500

    refactor: create MovementService.java

---
+++ src/main/java/service/MovementService.java
@@ -0,0 +1,34 @@
+package main.java.service;
+
+import main.java.map.Coordinates;
+import main.java.map.GameMap;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MovementService {
+    MovementOptionsProvider movementOptionsProvider;
+    PathFinder pathFinder;
+
+    public MovementService(MovementOptionsProvider movementOptionsProvider, PathFinder pathFinder) {
+        this.movementOptionsProvider = movementOptionsProvider;
+        this.pathFinder = pathFinder;
+    }
+
+    public Coordinates getNextCellForMove(GameMap gameMap, Class<?> target, Coordinates current) {
+        List<Coordinates> path = pathFinder.find(current, target, gameMap);
+        if (!path.isEmpty()) return path.getFirst();
+        return getRandomCell(current, target, gameMap);
+    }
+
+    private Coordinates getRandomCell(Coordinates current, Class<?> target, GameMap gameMap){
+        Random random = new Random();
+        List<Coordinates> availableCells;
+        availableCells = new ArrayList<>(movementOptionsProvider.getAvailableCellsForMove(current, target, gameMap));
+        if (!availableCells.isEmpty()){
+            return availableCells.get(random.nextInt(availableCells.size()));
+        }
+        return current;
+    }
+}
