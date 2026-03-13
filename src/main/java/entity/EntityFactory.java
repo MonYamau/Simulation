48d8505640b3464e058d06e88b7544b3ce4d7f39
MonYamau <White.Fox1825@yandex.ru>
2026-03-13 20:44:07 +0500

    refactor: change EntityFactory constructor

--- src/main/java/entity/EntityFactory.java
+++ src/main/java/entity/EntityFactory.java
@@ -2,10 +2,12 @@ package main.java.entity;
 
 import main.java.entity.creature.Cat;
 import main.java.entity.creature.Mouse;
+import main.java.map.Coordinates;
 import main.java.service.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Supplier;
 
 public class EntityFactory {
@@ -15,13 +17,20 @@ public class EntityFactory {
     public static final int MOUSE_SPEED = 2;
     public static final int CAT_ATTACK = 2;
 
+    Set<Coordinates> shifts = Set.of(
+            new Coordinates(-1, 0),
+                new Coordinates(1, 0),
+                new Coordinates(0, -1),
+                new Coordinates(0, 1)
+        );
+
     private final Map<Class<?>, Supplier<Entity>> entityCreators = new HashMap<>();
 
     public EntityFactory() {
-        PathFindingService bfsPathFinder = new BfsPathFinder();
-        FeedingService survivorFeeder = new SurvivorFeeder();
-        FeedingService predatorFeeder = new PredatorFeeder();
-        initializeEntityCreators(bfsPathFinder, survivorFeeder, predatorFeeder);
+        MovementOptionsProvider movementOptionsProvider = new MovementOptionsProvider(shifts);
+        PathFinder bfsPathFinder = new BfsPathFinder(movementOptionsProvider);
+        MovementService movementService = new MovementService(movementOptionsProvider, bfsPathFinder);
+        initializeEntityCreators(movementService);
     }
 
     public <T extends Entity> T createEntity(Class<T> entityClass) {
@@ -30,12 +39,12 @@ public class EntityFactory {
         return entityCreator.get();
     }
 
-    private void initializeEntityCreators(PathFindingService bfsPathFinder, FeedingService survivorFeeder, FeedingService predatorFeeder) {
+    private void initializeEntityCreators(MovementService movementService) {
         registerEntityCreator(Box.class, Box::new);
         registerEntityCreator(Basket.class, Basket::new);
         registerEntityCreator(Cheese.class, Cheese::new);
-        registerEntityCreator(Cat.class, () -> new Cat(CAT_HP, CAT_SPEED, Mouse.class, bfsPathFinder, predatorFeeder, CAT_ATTACK));
-        registerEntityCreator(Mouse.class, () -> new Mouse(MOUSE_HP, MOUSE_SPEED, Cheese.class, bfsPathFinder, survivorFeeder));
+        registerEntityCreator(Cat.class, () -> new Cat(CAT_HP, CAT_SPEED, Mouse.class, movementService, CAT_ATTACK));
+        registerEntityCreator(Mouse.class, () -> new Mouse(MOUSE_HP, MOUSE_SPEED, Cheese.class, movementService));
     }
 
     private <T extends Entity> void registerEntityCreator(Class<T> entityClass, Supplier<Entity> creator) {
