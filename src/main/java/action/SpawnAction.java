971554bde391ccbc8db365df70e80570277bf108
MonYamau <White.Fox1825@yandex.ru>
2026-03-14 21:00:54 +0500

    refactor: change values of constants

--- src/main/java/action/SpawnAction.java
+++ src/main/java/action/SpawnAction.java
@@ -7,8 +7,8 @@ public abstract class SpawnAction extends Action {
     protected static final int DEFAULT_CAT_COUNT = 3;
     protected static final int DEFAULT_MOUSE_COUNT = 5;
     protected static final int DEFAULT_CHEESE_COUNT = 7;
-    protected static final int DEFAULT_BOX_COUNT = 25;
-    protected static final int DEFAULT_BASKET_COUNT = 20;
+    protected static final int DEFAULT_BOX_COUNT = 23;
+    protected static final int DEFAULT_BASKET_COUNT = 15;
 
     protected EntityFactory entityFactory;
 
