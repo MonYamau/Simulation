47792270615ee7c54ba639a5eab49ad15465d0e3
MonYamau <White.Fox1825@yandex.ru>
2026-03-14 21:02:22 +0500

    refactor: add constants; change constructor

--- src/main/java/entity/EntityFactory.java
+++ src/main/java/entity/EntityFactory.java
@@ -2,37 +2,28 @@ package main.java.entity;
 
 import main.java.entity.creature.Cat;
 import main.java.entity.creature.Mouse;
-import main.java.map.Coordinates;
-import main.java.service.BfsPathFinder;
-import main.java.service.MovementOptionsProvider;
+import main.java.entity.creature.Survivor;
 import main.java.service.MovementService;
 import main.java.service.PathFinder;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 import java.util.function.Supplier;
 
 public class EntityFactory {
-    public static final int CAT_HP = 10;
-    public static final int MOUSE_HP = 6;
-    public static final int CAT_SPEED = 3;
-    public static final int MOUSE_SPEED = 2;
-    public static final int CAT_ATTACK = 2;
+    private static final int CAT_HP = 10;
+    private static final int MOUSE_HP = 6;
+    private static final Class<?> CAT_TYPE_OF_FOOD = Survivor.class;
+    private static final Class<?> MOUSE_TYPE_OF_FOOD = Cheese.class;
+    private static final int CAT_SPEED = 3;
+    private static final int MOUSE_SPEED = 2;
+    private static final int CAT_ATTACK = 2;
 
-    private final Map<Class<?>, Supplier<Entity>> entityCreators = new HashMap<>();
+    private final Map<Class<?>, Supplier<Entity>> entityCreators;
 
-    Set<Coordinates> shifts = Set.of(
-            new Coordinates(-1, 0),
-            new Coordinates(1, 0),
-            new Coordinates(0, -1),
-            new Coordinates(0, 1)
-    );
-
-    public EntityFactory() {
-        MovementOptionsProvider movementOptionsProvider = new MovementOptionsProvider(shifts);
-        PathFinder bfsPathFinder = new BfsPathFinder(movementOptionsProvider);
-        MovementService movementService = new MovementService(movementOptionsProvider, bfsPathFinder);
+    public EntityFactory(PathFinder pathFinder) {
+        entityCreators = new HashMap<>();
+        MovementService movementService = new MovementService(pathFinder);
         initializeEntityCreators(movementService);
     }
 
@@ -46,8 +37,8 @@ public class EntityFactory {
         registerEntityCreator(Box.class, Box::new);
         registerEntityCreator(Basket.class, Basket::new);
         registerEntityCreator(Cheese.class, Cheese::new);
-        registerEntityCreator(Cat.class, () -> new Cat(CAT_HP, CAT_SPEED, Mouse.class, movementService, CAT_ATTACK));
-        registerEntityCreator(Mouse.class, () -> new Mouse(MOUSE_HP, MOUSE_SPEED, Cheese.class, movementService));
+        registerEntityCreator(Cat.class, () -> new Cat(CAT_HP, CAT_SPEED, CAT_TYPE_OF_FOOD, movementService, CAT_ATTACK));
+        registerEntityCreator(Mouse.class, () -> new Mouse(MOUSE_HP, MOUSE_SPEED, MOUSE_TYPE_OF_FOOD, movementService));
     }
 
     private <T extends Entity> void registerEntityCreator(Class<T> entityClass, Supplier<Entity> creator) {
