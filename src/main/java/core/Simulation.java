be4932de9b02f94a3df0f893fe04f9170c0d9857
MonYamau <White.Fox1825@yandex.ru>
2026-03-14 21:13:37 +0500

    refactor: change constructor

--- src/main/java/core/Simulation.java
+++ src/main/java/core/Simulation.java
@@ -2,15 +2,16 @@ package main.java.core;
 
 import main.java.action.ActionManager;
 import main.java.map.GameMap;
+import main.java.map.GameMapRenderer;
 
 public class Simulation {
     TurnExecutor turnExecutor;
     SimulationThreadManager simulationThreadManager;
 
-    public Simulation(GameMap gameMap, ActionManager actionManager) {
-        this.turnExecutor = new TurnExecutor(gameMap, actionManager);
+    public Simulation(GameMap gameMap, GameMapRenderer gameMapRenderer, ActionManager actionManager) {
+        this.turnExecutor = new TurnExecutor(gameMap, actionManager, gameMapRenderer);
         this.simulationThreadManager = new SimulationThreadManager(turnExecutor);
-        SimulationCreator.init(gameMap, actionManager);
+        SimulationInitializer.init(gameMap, actionManager, gameMapRenderer);
     }
 
     public void executeTurn() {
