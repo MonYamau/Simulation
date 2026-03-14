1e291b6a892a94666d0058515db0c0a34f400172
MonYamau <White.Fox1825@yandex.ru>
2026-03-14 21:08:15 +0500

    refactor: create SimulationFactory.java

---
+++ src/main/java/core/SimulationFactory.java
@@ -0,0 +1,23 @@
+package main.java.core;
+
+import main.java.action.ActionManager;
+import main.java.entity.EntityFactory;
+import main.java.map.GameMap;
+import main.java.map.GameMapRenderer;
+import main.java.service.BfsPathFinder;
+import main.java.service.PathFinder;
+
+
+public class SimulationFactory {
+    private static final int DEFAULT_MAX_COLUMN_VALUE = 14;
+    private static final int DEFAULT_MAX_ROW_VALUE = 16;
+
+    public Simulation create(){
+        PathFinder bfsPathFinder = new BfsPathFinder();
+        EntityFactory entityFactory = new EntityFactory(bfsPathFinder);
+        GameMap gameMap = new GameMap(DEFAULT_MAX_COLUMN_VALUE, DEFAULT_MAX_ROW_VALUE);
+        GameMapRenderer gameMapRenderer = new GameMapRenderer(DEFAULT_MAX_COLUMN_VALUE, DEFAULT_MAX_ROW_VALUE);
+        ActionManager actionManager = new ActionManager(gameMap, entityFactory);
+        return new Simulation(gameMap, gameMapRenderer, actionManager);
+    }
+}
