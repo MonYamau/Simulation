fad51a39a5cdf59291bd5e2db0f117616a19c971
MonYamau <White.Fox1825@yandex.ru>
2026-03-18 15:02:31 +0500

    refactor: move method "getEmptyCells" at EntitySpawner.java

--- src/main/java/utils/EntitySpawner.java
+++ src/main/java/utils/EntitySpawner.java
@@ -5,6 +5,7 @@ import main.java.entity.EntityFactory;
 import main.java.gamemap.GameMap;
 import main.java.gamemap.Coordinates;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -20,8 +21,20 @@ public final class EntitySpawner {
 
     private static Coordinates getRandomEmptyCell(GameMap gameMap) {
         Random random = new Random();
-        List<Coordinates> emptyCells = gameMap.getEmptyCells();
+        List<Coordinates> emptyCells = getEmptyCells(gameMap);
         int randomIndex = random.nextInt(emptyCells.size());
         return emptyCells.get(randomIndex);
+    }
+
+    public static List<Coordinates> getEmptyCells(GameMap gameMap) {
+        List<Coordinates> emptyCells = new ArrayList<>();
+        for (int col = 0; col < gameMap.getHeight(); col++) {
+            for (int row = 0; row < gameMap.getWidth(); row++) {
+                if (gameMap.isCellEmpty(new Coordinates(col, row))) {
+                    emptyCells.add(new Coordinates(col, row));
+                }
+            }
+        }
+        return emptyCells;
     }
 }
