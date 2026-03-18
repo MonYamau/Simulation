d1cabab0dafea04121383427a2c3cdff30ce4cdb
MonYamau <White.Fox1825@yandex.ru>
2026-03-18 19:42:16 +0500

    fix: fix the initialization of action lists

--- src/main/java/core/SimulationFactory.java
+++ src/main/java/core/SimulationFactory.java
@@ -16,27 +16,27 @@ import java.util.List;
 public class SimulationFactory {
     private static final int DEFAULT_MAX_COLUMN_VALUE = 14;
     private static final int DEFAULT_MAX_ROW_VALUE = 16;
-    List<Action> initActions;
-    List<Action> turnActions;
 
     public Simulation create() {
-        List<Action> initActions = new ArrayList<>();
-        List<Action> turnActions = new ArrayList<>();
         PathFinder bfsPathFinder = new BfsPathFinder();
         EntityFactory entityFactory = new EntityFactory(bfsPathFinder);
         GameMap gameMap = new GameMap(DEFAULT_MAX_COLUMN_VALUE, DEFAULT_MAX_ROW_VALUE);
         GameMapRenderer gameMapRenderer = new GameMapRenderer();
-        fillInitActions(gameMap, entityFactory);
-        fillTurnActions(gameMap, entityFactory);
+        List<Action> initActions = getInitActions(gameMap, entityFactory);
+        List<Action> turnActions = getTurnActions(gameMap, entityFactory);
         return new Simulation(gameMap, gameMapRenderer, initActions, turnActions);
     }
 
-    private void fillInitActions(GameMap gameMap, EntityFactory entityFactory) {
+    private List<Action> getInitActions(GameMap gameMap, EntityFactory entityFactory) {
+        List<Action> initActions = new ArrayList<>();
         initActions.add(new GameMapInitializer(gameMap, entityFactory));
+        return initActions;
     }
 
-    private void fillTurnActions(GameMap gameMap, EntityFactory entityFactory) {
+    private List<Action> getTurnActions(GameMap gameMap, EntityFactory entityFactory) {
+        List<Action> turnActions = new ArrayList<>();
         turnActions.add(new ResourceProvider(gameMap, entityFactory));
         turnActions.add(new TurnMovement(gameMap));
+        return turnActions;
     }
 }
